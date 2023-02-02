349ba3c4f6b48a78f700c5800d67f31a1dda7584
ogimly <ogimly@gmail.com>
2023-02-02 23:33:31 +0500

    feat: add listen port from .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DEFAULT_PORT } from './common/consts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  await app.listen(process.env.PORT ?? DEFAULT_PORT);
 }
 bootstrap();
