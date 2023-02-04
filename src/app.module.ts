8649b8f1fbf6b8ca4e34fe282d0d21e7ef932cb5
ogimly <ogimly@gmail.com>
2023-02-04 15:24:56 +0500

    feat: generate tracks resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 // import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ArtistsModule } from './artists/artists.module';
+import { TracksModule } from './tracks/tracks.module';
 
 @Module({
-  imports: [UsersModule, ArtistsModule],
+  imports: [UsersModule, ArtistsModule, TracksModule],
   // controllers: [AppController],
   // providers: [AppService],
 })
