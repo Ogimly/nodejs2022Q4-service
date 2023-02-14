7f6a89d3baa2f992eaad2623471b06149c01ce8d
ogimly <ogimly@gmail.com>
2023-02-15 02:33:12 +0500

    feat: add artist migrations

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,15 +1,17 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { ArtistsService } from './artists.service';
 import { ArtistsController } from './artists.controller';
-import { TracksModule } from '../tracks/tracks.module';
-import { AlbumsModule } from '../albums/albums.module';
-import { FavoritesModule } from '../favorites/favorites.module';
+// import { TracksModule } from '../tracks/tracks.module';
+// import { AlbumsModule } from '../albums/albums.module';
+// import { FavoritesModule } from '../favorites/favorites.module';
+import { PrismaModule } from '../common/prisma/prisma.module';
 
 @Module({
   imports: [
-    forwardRef(() => TracksModule),
-    forwardRef(() => AlbumsModule),
-    forwardRef(() => FavoritesModule),
+    PrismaModule,
+    // forwardRef(() => TracksModule),
+    // forwardRef(() => AlbumsModule),
+    // forwardRef(() => FavoritesModule),
   ],
   controllers: [ArtistsController],
   providers: [ArtistsService],
--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -1,49 +1,112 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
-import { AlbumsService } from '../albums/albums.service';
-import { ArtistsRepository } from '../common/DB/artisrs-db';
-import { FavoritesService } from '../favorites/favorites.service';
-import { TracksService } from '../tracks/tracks.service';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { DBMessages } from '../common/enums';
+import { RequestResult } from '../common/interfaces';
+import { PrismaService } from '../common/prisma/prisma.service';
+// import { AlbumsService } from '../albums/albums.service';
+// import { ArtistsRepository } from '../common/DB/artisrs-db';
+// import { FavoritesService } from '../favorites/favorites.service';
+// import { TracksService } from '../tracks/tracks.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { ArtistEntity } from './entities/artist.entity';
 
 @Injectable()
 export class ArtistsService {
-  private artists: ArtistsRepository;
+  // private artists: ArtistsRepository;
 
-  constructor(
-    @Inject(forwardRef(() => TracksService))
-    private tracksService: TracksService,
+  // constructor(
+  //   @Inject(forwardRef(() => TracksService))
+  //   private tracksService: TracksService,
 
-    @Inject(forwardRef(() => AlbumsService))
-    private albumsService: AlbumsService,
+  //   @Inject(forwardRef(() => AlbumsService))
+  //   private albumsService: AlbumsService,
 
-    @Inject(forwardRef(() => FavoritesService))
-    private favoritesService: FavoritesService
-  ) {
-    this.artists = new ArtistsRepository();
+  //   @Inject(forwardRef(() => FavoritesService))
+  //   private favoritesService: FavoritesService
+  // ) {
+  //   this.artists = new ArtistsRepository();
+  // }
+  constructor(private prisma: PrismaService) {}
+
+  async create(createArtistDto: CreateArtistDto): Promise<RequestResult<ArtistEntity>> {
+    const newArtist = await this.prisma.artist.create({
+      data: createArtistDto,
+    });
+
+    return {
+      data: newArtist,
+      status: HttpStatus.CREATED,
+    };
   }
 
-  create(createArtistDto: CreateArtistDto) {
-    return this.artists.create(createArtistDto);
+  async findAll(ids: string[] = []): Promise<RequestResult<ArtistEntity[]>> {
+    // return this.prisma.artist.findAll(ids);
+    const artists = await this.prisma.artist.findMany();
+    return {
+      data: artists,
+      status: HttpStatus.OK,
+    };
   }
 
-  findAll(ids: string[] = []) {
-    return this.artists.findAll(ids);
+  async findOne(id: string): Promise<RequestResult<ArtistEntity>> {
+    const foundArtist = await this.prisma.artist.findUnique({ where: { id } });
+
+    if (!foundArtist)
+      return {
+        data: null,
+        status: HttpStatus.NOT_FOUND,
+        error: DBMessages.ArtistNotFound,
+      };
+
+    return {
+      data: foundArtist,
+      status: HttpStatus.OK,
+    };
   }
 
-  findOne(id: string) {
-    return this.artists.findOne(id);
+  async update(
+    id: string,
+    updateArtistDto: UpdateArtistDto
+  ): Promise<RequestResult<ArtistEntity>> {
+    const foundArtist = await this.prisma.artist.findUnique({ where: { id } });
+
+    if (!foundArtist)
+      return {
+        data: null,
+        status: HttpStatus.NOT_FOUND,
+        error: DBMessages.ArtistNotFound,
+      };
+
+    const updatedArtist = await this.prisma.artist.update({
+      where: { id },
+      data: updateArtistDto,
+    });
+
+    return {
+      data: updatedArtist,
+      status: HttpStatus.OK,
+    };
   }
 
-  update(id: string, updateArtistDto: UpdateArtistDto) {
-    return this.artists.update(id, updateArtistDto);
-  }
+  async remove(id: string): Promise<RequestResult<ArtistEntity>> {
+    // await this.tracksService.removeArtistId(id);
+    // await this.albumsService.removeArtistId(id);
+    // await this.favoritesService.removeArtist(id);
 
-  async remove(id: string) {
-    await this.tracksService.removeArtistId(id);
-    await this.albumsService.removeArtistId(id);
-    await this.favoritesService.removeArtist(id);
+    const foundArtist = await this.prisma.artist.findUnique({ where: { id } });
 
-    return this.artists.remove(id);
+    if (!foundArtist)
+      return {
+        data: null,
+        status: HttpStatus.NOT_FOUND,
+        error: DBMessages.ArtistNotFound,
+      };
+
+    await this.prisma.artist.delete({ where: { id } });
+
+    return {
+      data: null,
+      status: HttpStatus.NO_CONTENT,
+    };
   }
 }
