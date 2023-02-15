3fb36958af682c6082779056370fc6f1164503cd
ogimly <ogimly@gmail.com>
2023-02-15 22:24:44 +0500

    feat: add album migrations

--- src/albums/albums.module.ts
+++ src/albums/albums.module.ts
@@ -1,11 +1,15 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AlbumsService } from './albums.service';
 import { AlbumsController } from './albums.controller';
-import { TracksModule } from '../tracks/tracks.module';
-import { FavoritesModule } from '../favorites/favorites.module';
+// import { TracksModule } from '../tracks/tracks.module';
+// import { FavoritesModule } from '../favorites/favorites.module';
+import { PrismaModule } from '../common/prisma/prisma.module';
 
 @Module({
-  imports: [forwardRef(() => TracksModule), forwardRef(() => FavoritesModule)],
+  imports: [
+    PrismaModule,
+    // forwardRef(() => TracksModule), forwardRef(() => FavoritesModule)
+  ],
   controllers: [AlbumsController],
   providers: [AlbumsService],
   exports: [AlbumsService],
--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -1,48 +1,102 @@
-import { forwardRef, Injectable, Inject } from '@nestjs/common';
-import { AlbumsRepository } from '../common/DB/albums-db';
-import { FavoritesService } from '../favorites/favorites.service';
-import { TracksService } from '../tracks/tracks.service';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { DBMessages } from '../common/enums';
+import { PrismaService } from '../common/prisma/prisma.service';
+// import { AlbumsRepository } from '../common/DB/albums-db';
+// import { FavoritesService } from '../favorites/favorites.service';
+// import { TracksService } from '../tracks/tracks.service';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
 
 @Injectable()
 export class AlbumsService {
-  private albums: AlbumsRepository;
+  // private albums: AlbumsRepository;
 
-  constructor(
-    @Inject(forwardRef(() => TracksService))
-    private tracksService: TracksService,
+  // constructor(
+  //   @Inject(forwardRef(() => TracksService))
+  //   private tracksService: TracksService,
 
-    @Inject(forwardRef(() => FavoritesService))
-    private favoritesService: FavoritesService
-  ) {
-    this.albums = new AlbumsRepository();
+  //   @Inject(forwardRef(() => FavoritesService))
+  //   private favoritesService: FavoritesService
+  // ) {
+  //   this.albums = new AlbumsRepository();
+  // }
+  constructor(private prisma: PrismaService) {}
+
+  async create(createAlbumDto: CreateAlbumDto) {
+    const newAlbum = await this.prisma.album.create({
+      data: createAlbumDto,
+    });
+
+    return {
+      data: newAlbum,
+      status: HttpStatus.CREATED,
+    };
   }
 
-  create(createAlbumDto: CreateAlbumDto) {
-    return this.albums.create(createAlbumDto);
+  async findAll(ids: string[] = []) {
+    const albums = await this.prisma.album.findMany();
+    return {
+      data: albums,
+      status: HttpStatus.OK,
+    };
   }
 
-  findAll(ids: string[] = []) {
-    return this.albums.findAll(ids);
+  async findOne(id: string) {
+    const foundAlbum = await this.prisma.album.findUnique({ where: { id } });
+
+    if (!foundAlbum)
+      return {
+        data: null,
+        status: HttpStatus.NOT_FOUND,
+        error: DBMessages.AlbumNotFound,
+      };
+
+    return {
+      data: foundAlbum,
+      status: HttpStatus.OK,
+    };
   }
 
-  findOne(id: string) {
-    return this.albums.findOne(id);
-  }
+  async update(id: string, updateAlbumDto: UpdateAlbumDto) {
+    const foundAlbum = await this.prisma.album.findUnique({ where: { id } });
 
-  update(id: string, updateAlbumDto: UpdateAlbumDto) {
-    return this.albums.update(id, updateAlbumDto);
+    if (!foundAlbum)
+      return {
+        data: null,
+        status: HttpStatus.NOT_FOUND,
+        error: DBMessages.AlbumNotFound,
+      };
+
+    const updatedAlbum = await this.prisma.album.update({
+      where: { id },
+      data: updateAlbumDto,
+    });
+
+    return {
+      data: updatedAlbum,
+      status: HttpStatus.OK,
+    };
   }
 
   async remove(id: string) {
-    await this.tracksService.removeAlbumId(id);
-    await this.favoritesService.removeAlbum(id);
+    const foundAlbum = await this.prisma.album.findUnique({ where: { id } });
 
-    return this.albums.remove(id);
+    if (!foundAlbum)
+      return {
+        data: null,
+        status: HttpStatus.NOT_FOUND,
+        error: DBMessages.AlbumNotFound,
+      };
+
+    await this.prisma.album.delete({ where: { id } });
+
+    return {
+      data: null,
+      status: HttpStatus.NO_CONTENT,
+    };
   }
 
-  removeArtistId(artistId: string) {
-    return this.albums.removeArtistId(artistId);
-  }
+  // removeArtistId(artistId: string) {
+  //   return this.albums.removeArtistId(artistId);
+  // }
 }
